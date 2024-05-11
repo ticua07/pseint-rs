73ef00343e0299e619a053e185239364f46aeef7
ticua07 <ticua@ticua.ar>
2024-05-10 21:03:17 -0300

    remove lexer testing code as it is working properly now

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,20 @@
 use lexer::find_algorithm;
+use parser::{postfix_stack_evaluator, shunting_yard};
 
 use crate::lexer::Lexer;
 
 mod file;
 mod lexer;
+mod parser;
 mod tokens;
 
 fn main() {
-    let content = file::open_file("./algorithm.psc");
+    // let content = file::open_file("./algorithm.psc");
+    // let (algo_start, algo_end) = find_algorithm(&content);
+    // let _code = content[(algo_start + 1)..(algo_end - 1)].to_string();
 
-    let (algo_start, algo_end) = find_algorithm(&content);
-    let code = content[(algo_start + 1)..(algo_end - 1)].to_string();
-
-    // for line in code.split(";") {
-    // Lexer::lex(line.to_string());
-    // }
-
-    Lexer::lex("Definir algoritmo1 Como Caracter;".to_string());
-    Lexer::lex("variable = 'hola' hola".to_string());
-    Lexer::lex("1 + 1".to_string());
-    Lexer::lex("variable = ''".to_string());
-    Lexer::lex("variable = 123".to_string());
-    Lexer::lex("variable <- 123".to_string());
-
-    Lexer::lex("2 > 1".to_string());
-    Lexer::lex("1 < 2".to_string());
-    Lexer::lex("2 >= 2".to_string());
-    Lexer::lex("2 <= 2".to_string());
+    let tokens = Lexer::lex("(5*4+3*2)-1".to_string());
+    let postfix = shunting_yard(tokens);
+    let result = postfix_stack_evaluator(postfix);
+    println!("{}", result);
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = main(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
